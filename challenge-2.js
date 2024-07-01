c2dd359df0994cec62b89bc87da83e1424cb6603
Lewis Odero <lewisodero27@gmail.com>
2024-07-02 01:47:56 +0300

    Fix: confusion was made btwn challenge 1 and 2(corrected it)

--- challenge-2.js
+++ challenge-2.js
@@ -1,30 +1,24 @@
-// Function to calculate demerit points based on speed.
-function generateGrade(marks) {
-	if (marks < 0 || marks > 100) {
-		return "Invalid marks. Please enter a number between 0 and 100.";
-	}
+function checkSpeed(speed) {
+	const speedLimit = 70;
+	const kmPerDemeritPoint = 5;
 
-	let grade;
-	if (marks > 79) {
-		grade = "A";
-	} else if (marks >= 60) {
-		grade = "B";
-	} else if (marks >= 49) {
-		grade = "C";
-	} else if (marks >= 40) {
-		grade = "D";
+	if (speed <= speedLimit) {
+		return "Ok";
 	} else {
-		grade = "E";
+		const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
+		if (demeritPoints > 12) {
+			return "License suspended";
+		} else {
+			return `Points: ${demeritPoints}`;
+		}
 	}
-	return grade;
 }
 
-function promptGrade() {
-	const marks = prompt("Enter the student's marks (0-100):");
-	const grade = generateGrade(Number(marks));
-	if (grade.startsWith("Invalid")) {
-		alert(grade);
+function promptSpeed() {
+	let speed = parseFloat(prompt("Enter the speed:"));
+	if (isNaN(speed) || speed < 0) {
+		alert("Please enter a valid positive number.");
 	} else {
-		alert(`The student's grade is: ${grade}`);
+		alert(checkSpeed(speed));
 	}
 }
